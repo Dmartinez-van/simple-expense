4ee83ab7cf4ce8bf9b12bdeaab16278c22588864
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-07-27 19:12:37 -0700

    Testing with cost datatypes - ended on INT (wanted 2 decimal places only)

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+-- Drop existing database of same name (IF IT EXISTS)
+DROP DATABASE expense_track_db;
+
 CREATE DATABASE expense_track_db;
 
 \c expense_track_db
@@ -6,14 +9,16 @@ CREATE TABLE expenses(
   expense_id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   category VARCHAR(255),
-  amount INT
+  cost INT
 );
 
-INSERT INTO expenses (name, category, amount) VALUES 
+INSERT INTO expenses (name, category, cost) VALUES 
 ('Breakfast', 'Food', 20),
 ('Lunch', 'Food',13),
 ('Dinner', 'Food',30),
 ('Snack', 'Food',5),
 ('Desert', 'Food',10),
 ('New Car', 'Transportation', 20000),
-('Uber Ride', 'Transportation', 17);
+('Uber Ride', 'Transportation', 17.534);
+
+SELECT * FROM expenses;
